bdc2e9fae48cdb987ee5080628c6613f55971aa4
mw08081 <president304@naver.com>
2021-11-16 21:48:15 +0900

    added SceneController v2

--- Assets/source/cs/Scene/Stage2Scene.cs
+++ Assets/source/cs/Scene/Stage2Scene.cs
@@ -6,6 +6,6 @@ public class Stage2Scene : BaseScene
 {
     protected override void Initializing()
     {
-        Debug.Log(SystemManager.Instance.PlayerHp + " " + SystemManager.Instance.ScoreSystem.Score);
+        
     }
 }
